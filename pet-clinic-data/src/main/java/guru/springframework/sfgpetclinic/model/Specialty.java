612d99ded76c8b4ee49599191c2e9f6381f4e2b2
Phil <philstoll@gmail.com>
2018-11-01 16:51:50 -0500

    Convert Vet Entity to JPA mappings. closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -1,8 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
-import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
