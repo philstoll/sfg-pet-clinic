5817b1da94100d0a6898f9f83a62a9f3ee250e8d
Phil <philstoll@gmail.com>
2018-11-01 22:41:58 -0500

    add jpa service for vet. closes #41

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetJpaService.java
@@ -0,0 +1,54 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Vet findByLastName(String lastName) {
+        return this.vetRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+
+        this.vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return this.vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return this.vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        this.vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        this.vetRepository.deleteById(aLong );
+    }
+}
