7027d354e01897dd378b14dec0ebe00b9d6d629c
Phil <philstoll@gmail.com>
2018-11-01 23:11:33 -0500

    add jpa service for Visit. closes #47

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitJpaService.java
@@ -0,0 +1,49 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.repositories.VisitRepository;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        this.visitRepository.findAll().forEach(visits::add);
+
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return this.visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return this.visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        this.visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        this.visitRepository.deleteById(aLong);
+    }
+}
